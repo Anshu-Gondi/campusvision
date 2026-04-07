c8e3c4d4a53fd31a6dd113d26609a781fb994d01
Anshu-Gondi <agondi982@gmail.com>
2026-04-07 17:19:57 +0530

    Save work before cargo fix

--- Backend/attendance_backend/attendance_backend/settings.py
+++ Backend/attendance_backend/attendance_backend/settings.py
@@ -27,6 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 ADMIN_JWT_SECRET = config("ADMIN_JWT_SECRET")
 SECRET_ENCRYPTION_KEY = config("SECRET_ENCRYPTION_KEY").encode()
+APP_JWT_SECRET = config("APP_JWT_SECRET")
+APP_JWT_EXPIRY_HOURS = config("APP_JWT_EXPIRY_HOURS", default=24, cast=int)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
